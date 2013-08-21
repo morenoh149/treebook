66f3cfe1edbd26e997954fcf7ce64c5a9e09b0d5
Harry Moreno <morenoh149@gmail.com>
2013-08-21 13:41:39 -0700

    added devise views to project, added user model to database

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # attr_accessible :title, :body
+  # Setup accessible (or protected) attributes for your model
+  # http://stackoverflow.com/questions/10796092/cant-mass-assign-protected-attributes-first-name-last-name-email-password
+  attr_accessible :email, :password, :password_confirmation,
+                  :first_name, :last_name, :profile_name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Treebook::Application.routes.draw do
-  resources :statuses
+  devise_for :users
 
+  resources :statuses
+  root to: 'statuses#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
