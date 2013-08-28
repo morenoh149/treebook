94ca31291304ef5ced06a73338a8153a046f888e
Harry Moreno <morenoh149@gmail.com>
2013-08-27 23:03:12 -0700

    changed flash error display logic, added a helper for handling flash css selectors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_class(type)
+    case type
+    when :alert
+      "alert-danger"
+    when :notice
+      "alert-info"
+    else
+      ""
+    end
+  end
 end
