581044c372219bd20f784f8ce54b00805837088a
Harry Moreno <morenoh149@gmail.com>
2013-09-02 23:04:49 -0700

    Added status validations and tests

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,5 @@
 class StatusesController < ApplicationController
-  before_filter :authenticate_user!, only: [:new]
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit]
   # GET /statuses
   # GET /statuses.json
   def index
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test "that a status's content is at least 2 letters" do
+    status = Status.new
+    status.content = "h"
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user id" do
+    status = Status.new
+    status.content = "hello"
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+  end
 end
