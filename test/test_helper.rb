171f46b73d4ad5ea0bfe4473e0d4a717e43df906
Harry Moreno <morenoh149@gmail.com>
2013-09-02 22:40:33 -0700

    Added test, custom routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,8 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# Devise test helper
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
